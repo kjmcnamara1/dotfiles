d92f823819fc23d191a3e965fbc3b96b8d0cc5b3
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-14 21:53:47 -0600

    add keymaps for cmp and snippet jumping

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,11 +1,11 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    event = 'InsertEnter',
+    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-buffer",           -- source for text in buffer
       "hrsh7th/cmp-path",             -- source for file system paths
-      'hrsh7th/cmp-cmdline',          -- source for commandline
+      "hrsh7th/cmp-cmdline",          -- source for commandline
       "hrsh7th/cmp-nvim-lsp",         -- source for neovim builtin LSP client
       "L3MON4D3/LuaSnip",             -- snippet engine
       "saadparwaiz1/cmp_luasnip",     -- for autocompletion
@@ -15,19 +15,24 @@ return {
     },
     config = function(_, opts)
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
       require("luasnip.loaders.from_vscode").lazy_load()
 
-
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       cmp.setup({
         completion = {
-          completeopt = 'menu,menuone,preview,noselect',
+          completeopt = "menu,menuone,preview,noselect",
         },
         -- configure how nvim-cmp interacts with snippet engine
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
           end,
         },
         window = {
@@ -35,21 +40,43 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),            -- show completion suggestions
-          -- ["<C-Space>"] = function(fallback)
-          --   if cmp.visible() then
-          --     if cmp.visible_docs() then
-          --       cmp.close_docs()
-          --     else
-          --       cmp.open_docs()
-          --     end
-          --   else
-          --     cmp.complete()
-          --   end
-          -- end,
-          ["<C-x>"] = cmp.mapping.abort(),                   -- close completion window
+          -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping(
+            function(fallback)
+              if cmp.visible() then
+                cmp.close()
+              else
+                cmp.complete()
+              end
+            end),
+          ["<tab>"] = cmp.mapping(
+            function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end,
+            { "i", "s" }
+          ),
+          ["<s-tab>"] = cmp.mapping(
+            function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end,
+            { "i", "s" }
+          ),
+          -- ["<C-x>"] = cmp.mapping.abort(),                   -- close completion window
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         -- sources for autocompletion
@@ -61,14 +88,14 @@ return {
         }),
         -- configure lspkind for vs-code like pictograms in completion menu
         formatting = {
-          format = require('lspkind').cmp_format({
+          format = require("lspkind").cmp_format({
             maxwidth = 50,
             ellipsis_char = "...",
           }),
         },
       })
 
-      cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
+      cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
     end,
   },
   {
@@ -77,8 +104,8 @@ return {
     opts = {
       check_ts = true,
       ts_config = {
-        lua = { 'string' },
-        javascript = { 'template_string' },
+        lua = { "string" },
+        javascript = { "template_string" },
         java = false,
       },
     },
