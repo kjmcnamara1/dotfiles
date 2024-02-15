a08a3228940afb8019979a01018c961b6c253a6e
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-14 21:59:24 -0600

    start looping though items

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-ln -s "$(pwd)/.gitconfig" ~/.gitconfig
-ln -sd "$(pwd)/.config/nvim" ~/.config/nvim
+# ln -s "$(pwd)/.gitconfig" ~/.gitconfig
+# ln -sd "$(pwd)/.config/nvim" ~/.config/nvim
+
+for file in .*; do
+  if [ "$file" = '.git' ]; then
+    continue
+  fi
+  echo "$file"
+done
+
