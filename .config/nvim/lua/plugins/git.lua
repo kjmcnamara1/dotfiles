ad6a656420264111d42737537fa2226d3f405f38
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-19 16:59:12 -0600

    add fugitive and rhubarb, add keymaps for git diff and quit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "tpope/vim-fugitive",
+    cmd = { "G", "Git" },
+  },
+  {
+    "tpope/vim-rhubarb",
+    dependencies = "tpope/vim-fugitive",
+    cmd = "GBrowse"
+  },
+  {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim",         -- required
@@ -11,6 +20,8 @@ return {
       { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open NeoGit" }
     },
     opts = {
+      kind             = "split",
+      disable_hint     = true,
       graph_style      = "unicode",
       telescope_sorter = function()
         return require("telescope").extensions.fzf.native_fzf_sorter()
@@ -21,32 +32,55 @@ return {
         item = { "󰬪", "󰬧" },
         hunk = { "󰅂", "󰅀" },
       },
+      -- integrations     = {
+      --   telescope = true,
+      --   diffview = true,
+      -- },
     },
+    -- config = function(_, opts)
+    --   require("neogit").setup(opts)
+    --   -- Open Neogit window to left and set width=40cols
+    --   vim.api.nvim_create_autocmd("FileType", {
+    --     group = vim.api.nvim_create_augroup("neogit_status", { clear = true }),
+    --     pattern = "NeogitStatus",
+    --     callback = function()
+    --       vim.bo.bufhidden = "unload"
+    --       vim.cmd.wincmd("H")
+    --       vim.api.nvim_win_set_width(0, 35)
+    --     end
+    --   })
+    -- end
   },
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    keys = {
+      { "<leader>gp",  "<cmd>Gitsigns preview_hunk<cr>",                                     desc = "Git Preview Hunk",        buffer = 0 },
+      { "<leader>gi",  "<cmd>Gitsigns preview_hunk_inline<cr>",                              desc = "Git Preview Hunk Inline", buffer = 0 },
+      { "<leader>gd",  "<cmd>Gitsigns diffthis<cr>",                                         desc = "Git Diff Current File",   buffer = 0 },
+      { "q",           function() return vim.wo.diff and "<cmd>wincmd h | q<cr>" or "q" end, desc = "Close Git Diff",          buffer = 0, expr = true, },
+      { "<leader>gb",  "<cmd>Gitsigns toggle_current_line_blame<cr>",                        desc = "Toggle Git Blame",        buffer = 0 },
+      { "<leader>gS",  "<cmd>Gitsigns stage_buffer<cr>",                                     desc = "Git Stage Buffer",        buffer = 0 },
+      { "<leader>gR",  "<cmd>Gitsigns reset_buffer<cr>",                                     desc = "Git Reset Buffer",        buffer = 0 },
+      -- Git Hunks
+      { "<leader>ghs", "<cmd>Gitsigns stage_hunk<cr>",                                       desc = "Stage Git Hunk",          buffer = 0 },
+      { "<leader>ghr", "<cmd>Gitsigns reset_hunk<cr>",                                       desc = "Reset Git Hunk",          buffer = 0 },
+      { "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<cr>",                                  desc = "Undo Stage Git Hunk",     buffer = 0 },
+      -- Movements
+      { "]h",          "<cmd>Gitsigns next_hunk<cr>",                                        desc = "Next Git Hunk",           buffer = 0 },
+      { "[h",          "<cmd>Gitsigns prev_hunk<cr>",                                        desc = "Previous Git Hunk",       buffer = 0 },
+      -- Text Object
+      { "ih",          "<cmd>Gitsigns select_hunk<cr>",                                      desc = "Previous Git Hunk",       buffer = 0, mode = { "o", "x" } },
+      { "ah",          "<cmd>Gitsigns select_hunk<cr>",                                      desc = "Previous Git Hunk",       buffer = 0, mode = { "o", "x" } },
+    },
     opts = {
       preview_config = {
         border = "rounded",
       },
     },
-    keys = {
-      { "<leader>gp",  "<cmd>Gitsigns preview_hunk<cr>",              desc = "Git Preview Hunk",        buffer = 0 },
-      { "<leader>gi",  "<cmd>Gitsigns preview_hunk_inline<cr>",       desc = "Git Preview Hunk Inline", buffer = 0 },
-      { "<leader>gb",  "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle Git Blame",        buffer = 0 },
-      { "<leader>gS",  "<cmd>Gitsigns stage_buffer<cr>",              desc = "Git Stage Buffer",        buffer = 0 },
-      { "<leader>gR",  "<cmd>Gitsigns reset_buffer<cr>",              desc = "Git Reset Buffer",        buffer = 0 },
-      -- Git Hunks
-      { "<leader>ghs", "<cmd>Gitsigns stage_hunk<cr>",                desc = "Stage Git Hunk",          buffer = 0 },
-      { "<leader>ghr", "<cmd>Gitsigns reset_hunk<cr>",                desc = "Reset Git Hunk",          buffer = 0 },
-      { "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<cr>",           desc = "Undo Stage Git Hunk",     buffer = 0 },
-      -- Movements
-      { "]h",          "<cmd>Gitsigns next_hunk<cr>",                 desc = "Next Git Hunk",           buffer = 0 },
-      { "[h",          "<cmd>Gitsigns prev_hunk<cr>",                 desc = "Previous Git Hunk",       buffer = 0 },
-      -- Text Object
-      { "ih",          ":<c-u>Gitsigns select_hunk<cr>",              mode = { "o", "x" },              desc = "Previous Git Hunk", buffer = 0 },
-      { "ah",          ":<c-u>Gitsigns select_hunk<cr>",              mode = { "o", "x" },              desc = "Previous Git Hunk", buffer = 0 },
-    },
+    -- config = function(_, opts)
+    --   require("gitsigns").setup(opts)
+    --   vim.keymap.set("n", "q", function() return vim.wo.diff and "<cmd>wincmd h | q<cr>" or "q" end, {})
+    -- end
   },
 }
