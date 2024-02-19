b1bc50a9dd12931f40566797a5f724883b16f69a
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-19 16:54:21 -0600

    move folding options into ufo config

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -33,6 +33,9 @@ vim.opt.listchars = "trail:∙,extends:󰇘,precedes:󰇘,conceal:,nbsp:␣"
 -- Place a column line
 -- vim.opt.colorcolumn = '80'
 
+-- Allow signcolumn to accomodate gitsigns and lsp diagnostics
+vim.opt.signcolumn = "auto:2"
+
 -- Vim Autoformatting Options
 -- vim.g.autoformat = true
 -- vim.opt.formatoptions = "jcroqlnt" -- tcqj
@@ -61,11 +64,6 @@ vim.opt.sidescrolloff = 5
 
 -- vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
 
--- Folding options for ufo plugin
-vim.opt.foldcolumn = "auto:9"
--- vim.opt.foldlevel = 99
--- vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.fillchars = {
