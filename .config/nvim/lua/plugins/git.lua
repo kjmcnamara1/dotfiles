dfce5ce4621f65cdbfb05fb1afb8239182fc2b32
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-20 17:46:38 -0600

    add keymap to go directly to Diffview

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -14,9 +14,14 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      {
+        "sindrets/diffview.nvim",
+        keys = {
+          { "<leader>gG", "<cmd>DiffviewOpen<cr>", desc = "Open Git Diffs" },
+        },
+      },
     },
     cmd = "Neogit",
     keys = {
