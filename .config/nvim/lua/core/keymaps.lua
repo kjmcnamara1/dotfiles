86b510ce6b05730856422ae4a56684587455b560
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-20 16:20:18 -0600

    change quit keymap to double c-q

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -11,7 +11,7 @@ map("n", "<leader>gd", "<c-w>s<c-w>T<cmd>Gitsigns diffthis<cr>")
 map("i", "jk", "<esc>", { desc = "Exit insert mode" })
 
 -- Quit NeoVim
-map("n", "<c-q>", "<cmd>qa<cr>", { desc = "Quit NeoVim" })
+map("n", "<c-q><c-q>", "<cmd>qa<cr>", { desc = "Quit NeoVim" })
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
