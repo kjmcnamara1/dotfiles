d368b1e566d9ca8885858ec800d77a049c34f498
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-20 16:42:20 -0600

    keymap fugitive git commit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,6 +2,9 @@ return {
   {
     "tpope/vim-fugitive",
     cmd = { "G", "Git" },
+    keys = {
+      { "<leader>gc", "<cmd>G commit<cr>" }
+    }
   },
   {
     "tpope/vim-rhubarb",
@@ -55,17 +58,19 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     keys = {
-      { "<leader>gp",  "<cmd>Gitsigns preview_hunk<cr>",                                     desc = "Git Preview Hunk",        buffer = 0 },
-      { "<leader>gi",  "<cmd>Gitsigns preview_hunk_inline<cr>",                              desc = "Git Preview Hunk Inline", buffer = 0 },
-      { "<leader>gd",  "<cmd>Gitsigns diffthis<cr>",                                         desc = "Git Diff Current File",   buffer = 0 },
-      { "q",           function() return vim.wo.diff and "<cmd>wincmd h | q<cr>" or "q" end, desc = "Close Git Diff",          buffer = 0, expr = true, },
-      { "<leader>gb",  "<cmd>Gitsigns toggle_current_line_blame<cr>",                        desc = "Toggle Git Blame",        buffer = 0 },
-      { "<leader>gS",  "<cmd>Gitsigns stage_buffer<cr>",                                     desc = "Git Stage Buffer",        buffer = 0 },
-      { "<leader>gR",  "<cmd>Gitsigns reset_buffer<cr>",                                     desc = "Git Reset Buffer",        buffer = 0 },
+      { "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>",                                     desc = "Git Preview Hunk",               buffer = 0 },
+      { "<leader>gi", "<cmd>Gitsigns preview_hunk_inline<cr>",                              desc = "Git Preview Hunk Inline",        buffer = 0 },
+      { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>",                        desc = "Toggle Git Blame",               buffer = 0 },
+      { "<leader>gw", "<cmd>Gitsigns toggle_word_diff<cr>",                                 desc = "Toggle Git Word Diff",           buffer = 0 },
+      { "<leader>gS", "<cmd>Gitsigns stage_buffer<cr>",                                     desc = "Git Stage Buffer",               buffer = 0 },
+      { "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>",                                     desc = "Git Reset Buffer",               buffer = 0 },
+      { "<leader>gd", "<cmd>Gitsigns diffthis<cr>",                                         desc = "Git Diff Current File to Index", buffer = 0 },
+      { "<leader>gD", "<cmd>Gitsigns diffthis ~<cr>",                                       desc = "Git Diff Current File to HEAD",  buffer = 0 },
+      { "q",          function() return vim.wo.diff and "<cmd>wincmd h | q<cr>" or "q" end, desc = "Close Git Diff",                 buffer = 0, expr = true, },
       -- Git Hunks
-      { "<leader>ghs", "<cmd>Gitsigns stage_hunk<cr>",                                       desc = "Stage Git Hunk",          buffer = 0 },
-      { "<leader>ghr", "<cmd>Gitsigns reset_hunk<cr>",                                       desc = "Reset Git Hunk",          buffer = 0 },
-      { "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<cr>",                                  desc = "Undo Stage Git Hunk",     buffer = 0 },
+      { "<leader>hs", "<cmd>Gitsigns stage_hunk<cr>",                                       desc = "Stage Git Hunk",                 buffer = 0 },
+      { "<leader>hr", "<cmd>Gitsigns reset_hunk<cr>",                                       desc = "Reset Git Hunk",                 buffer = 0 },
+      { "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<cr>",                                  desc = "Undo Stage Git Hunk",            buffer = 0 },
       -- Movements
       { "]h",          "<cmd>Gitsigns next_hunk<cr>",                                        desc = "Next Git Hunk",           buffer = 0 },
       { "[h",          "<cmd>Gitsigns prev_hunk<cr>",                                        desc = "Previous Git Hunk",       buffer = 0 },
