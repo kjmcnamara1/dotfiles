e0c8b9ac2e9178f194dc54c2d013dd4bc98e96af
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-21 15:47:26 -0600

    try possession.nvim for session management

--- .config/nvim/lua/plugins/sessions.lua
+++ .config/nvim/lua/plugins/sessions.lua
@@ -1,21 +1,37 @@
 return {
-  "rmagatti/auto-session",
-  event = "VimEnter",
-  cmd = { "SessionSave", "SessionRestore", "SessionRestoreFromFile", "SessionDelete", "Autosession" },
-  keys = {
-    {
-      "<leader>ss",
-      function()
-        require("auto-session.session-lens").search_session()
-      end,
-      desc = "Sessions"
+  {
+    "rmagatti/auto-session",
+    -- enabled = false,
+    dependencies = { "tiagovla/scope.nvim", config = true, },
+    event = "VimEnter",
+    cmd = { "SessionSave", "SessionRestore", "SessionRestoreFromFile", "SessionDelete", "Autosession" },
+    keys = {
+      {
+        "<leader>ss",
+        function()
+          require("auto-session.session-lens").search_session()
+        end,
+        desc = "Sessions"
+      }
+    },
+    opts = {
+      -- auto_restore_enabled = false,
+      session_lens = {
+        load_on_setup = true,
+      },
+      pre_save_cmds = { "Neotree close", "TroubleClose", --[[ "ScopeSaveState" ]] },
+      -- post_restore_cmds = { "ScopeLoadState" },
     }
   },
-  opts = {
-    -- auto_restore_enabled = false,
-    session_lens = {
-      load_on_setup = true,
+  {
+    "jedrzejboczar/possession.nvim",
+    enabled=false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      autosave = {
+        current = true,
+      },
     },
-    pre_save_cmds = { "Neotree close", "TroubleClose" },
-  }
+
+  },
 }
