07e60c59f32530e6f0447007b61f8ba79cc825d1
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-23 15:21:34 -0600

    inverse hl match_paren and other colorscheme options for wezterm

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,21 @@
 return {
-
   {
     "rmehri01/onenord.nvim",
     name = "onenord",
     event = "VimEnter",
     priority = 1000,
     opts = {
-      fade_nc = true,
+      -- fade_nc = true,
       styles = {
-        -- comments = "italic",
+        comments = "italic",
         keywords = "bold",
+      },
+      disable = {
+        background = true,
+        float_background = true,
+      },
+      inverse = {
+        match_paren = true,
       },
       -- TODO: Need to add override for Neo-tree focused file 'NeoTreeCursorLine'
     },
