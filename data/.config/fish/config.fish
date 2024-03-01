161ff405ab45e1c39ec76c0ec730dd95dd1aec57
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-03-01 16:26:51 -0600

    reset fish config

--- data/.config/fish/config.fish
+++ data/.config/fish/config.fish
@@ -2,6 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Plugins
+# fisher plugin manager
+# curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+# fisher install oh-my-fish/plugin-sudope
+# fisher install acomagu/fish-async-prompt
+
+
 # <c-c> clears the commandline when in insert mode
 bind --mode insert \cc 'commandline -r ""'
 
