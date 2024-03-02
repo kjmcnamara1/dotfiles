c948d1150fcf51fb0db322dd16771373e94a5636
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-03-02 09:34:39 -0600

    use relative size for float term

--- data/.config/nvim/lua/plugins/terminal.lua
+++ data/.config/nvim/lua/plugins/terminal.lua
@@ -12,14 +12,14 @@ return {
       },
       float_opts = {
         border = "curved",
-        width = 120,
-        height = 30,
+        width = math.floor(vim.o.columns * .8),
+        height = math.floor(vim.o.lines * .6),
       },
       size = function(term)
         if term.direction == "horizontal" then
           return 15
         elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
+          return math.floor(vim.o.columns * 0.4)
         else
           return 20
         end
@@ -31,12 +31,12 @@ return {
         -- "<cmd>ToggleTerm<cr>",
         mode = { "i", "n", "t" },
         desc = "Toggle Terminal",
-        },
-        { "<c-t>f", "<cmd>ToggleTerm direction=float<cr>", mode = { "i", "n", "t" }, desc = "Toggle Float Terminal" },
-        { "<c-t>h", "<cmd>ToggleTerm direction=horizontal<cr>", mode = { "i", "n", "t" }, desc = "Toggle Horizontal Terminal", },
-        { "<c-t>v", "<cmd>ToggleTerm direction=vertical<cr>", mode = { "i", "n", "t" }, desc = "Toggle Vertical Terminal", },
-        { "<c-t>t", "<cmd>ToggleTerm direction=tab<cr>", mode = { "i", "n", "t" }, desc = "Toggle Tab Terminal" },
-        { "<c-x>", [[<c-\><c-n>]], mode = "t", buffer = 0 },
       },
+      { "<c-t>f", "<cmd>ToggleTerm direction=float<cr>",      mode = { "i", "n", "t" }, desc = "Toggle Float Terminal" },
+      { "<c-t>h", "<cmd>ToggleTerm direction=horizontal<cr>", mode = { "i", "n", "t" }, desc = "Toggle Horizontal Terminal", },
+      { "<c-t>v", "<cmd>ToggleTerm direction=vertical<cr>",   mode = { "i", "n", "t" }, desc = "Toggle Vertical Terminal", },
+      { "<c-t>t", "<cmd>ToggleTerm direction=tab<cr>",        mode = { "i", "n", "t" }, desc = "Toggle Tab Terminal" },
+      { "<c-x>",  [[<c-\><c-n>]],                             mode = "t",               buffer = 0 },
     },
+  },
 }
