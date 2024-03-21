6caa9331010b96ac7ab4eb37f38323f65f61510a
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-03-21 15:26:39 -0500

    add neorg and zenmode plugins

--- data/.config/nvim/lua/plugins/ui.lua
+++ data/.config/nvim/lua/plugins/ui.lua
@@ -21,4 +21,35 @@ return {
       { "<F3>", mode = { "i", "n", "v" }, desc = "Toggle maximize window" },
     },
   },
+  {
+    "folke/zen-mode.nvim",
+    dependencies = { "folke/twilight.nvim" },
+    keys = {
+      { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" }
+    },
+    opts = {
+      window = {
+        options = {
+          -- signcolumn = "no",
+          number = false,
+          relativenumber = false,
+          cursorline = false,
+          cursorcolumn = false,
+          foldcolumn = "0",
+          list = false,
+          scrolloff = 999,
+        }
+      },
+      plugins = {
+        options = {
+          enabled = true,
+          laststatus = 0,
+        },
+        twilight = { enabled = true },
+        gitsigns = { enabled = true },
+        tmux = { enabled = true },
+        wezterm = { enabled = true, font = "+2" },
+      },
+    }
+  },
 }
