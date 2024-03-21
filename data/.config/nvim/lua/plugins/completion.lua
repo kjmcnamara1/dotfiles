e995125a9adc554f28a030f016556c4b1cf495cb
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-03-21 15:24:48 -0500

    add several extra completion sources

--- data/.config/nvim/lua/plugins/completion.lua
+++ data/.config/nvim/lua/plugins/completion.lua
@@ -2,30 +2,29 @@ return {
   {
     "hrsh7th/nvim-cmp", -- autocompletion plugin
     cond = not vim.g.vscode,
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
-      "Exafunction/codeium.nvim",     -- copilot alternative
-      "hrsh7th/cmp-buffer",           -- completion source for text in buffer
-      "hrsh7th/cmp-path",             -- completion source for file system paths
-      "hrsh7th/cmp-cmdline",          -- completion source for commandline
-      "hrsh7th/cmp-nvim-lsp",         -- completion source for neovim builtin LSP client
-      "saadparwaiz1/cmp_luasnip",     -- completion source for autocompletion
-      "L3MON4D3/LuaSnip",             -- snippet engine
-      "rafamadriz/friendly-snippets", -- useful snippets
-      "onsails/lspkind.nvim",         -- vs-code like pictograms
-      "windwp/nvim-autopairs",        -- auto-close brackets and functions
+      "Exafunction/codeium.nvim",            -- copilot alternative
+      "hrsh7th/cmp-buffer",                  -- completion source for text in buffer
+      "hrsh7th/cmp-path",                    -- completion source for file system paths
+      "hrsh7th/cmp-cmdline",                 -- completion source for commandline
+      "petertriho/cmp-git",                  -- Git commits, github/gitlab issues, merge/pull requests, mentions, etc.
+      "mtoohey31/cmp-fish",                  -- fish shell
+      "andersevenrud/cmp-tmux",              -- tmux
+      "hrsh7th/cmp-nvim-lsp",                -- completion source for neovim builtin LSP client
+      "hrsh7th/cmp-nvim-lsp-signature-help", -- signature help
+      "saadparwaiz1/cmp_luasnip",            -- completion source for luasnip
+      "L3MON4D3/LuaSnip",                    -- snippet engine
+      "rafamadriz/friendly-snippets",        -- useful snippets
+      "onsails/lspkind.nvim",                -- vs-code like pictograms
+      "windwp/nvim-autopairs",               -- auto-close brackets and functions
+      "chrisgrieser/cmp-nerdfont",           -- nerd font icons
     },
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
 
       require("luasnip.loaders.from_vscode").lazy_load()
-
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
 
       cmp.setup({
         completion = {
@@ -42,8 +41,6 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping(
             function(fallback)
               if cmp.visible() then
@@ -52,33 +49,6 @@ return {
                 cmp.complete()
               end
             end),
-          -- ["<tab>"] = cmp.mapping(
-          --   function(fallback)
-          --     if cmp.visible() then
-          --       cmp.select_next_item()
-          --     elseif luasnip.expand_or_jumpable() then
-          --       luasnip.expand_or_jump()
-          --     elseif has_words_before() then
-          --       cmp.complete()
-          --     else
-          --       fallback()
-          --     end
-          --   end,
-          --   { "i", "s" }
-          -- ),
-          -- ["<s-tab>"] = cmp.mapping(
-          --   function(fallback)
-          --     if cmp.visible() then
-          --       cmp.select_prev_item()
-          --     elseif luasnip.jumpable(-1) then
-          --       luasnip.jump(-1)
-          --     else
-          --       fallback()
-          --     end
-          --   end,
-          --   { "i", "s" }
-          -- ),
-          -- ["<C-x>"] = cmp.mapping.abort(),                   -- close completion window
           ["<c-i>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -86,26 +56,81 @@ return {
         }),
         -- sources for autocompletion
         sources = cmp.config.sources({
-          { name = "codeium", group_index = 1, priority = 100, }, -- ai autocompletion
-          { name = "nvim_lsp" },                                  -- lsp
-          { name = "luasnip" },                                   -- snippets
-          { name = "buffer" },                                    -- text within current buffer
-          { name = "path" },                                      -- file system paths
+          { name = "codeium",                group_index = 1, priority = 100, }, -- ai autocompletion
+          { name = "nvim_lsp" },                                                 -- lsp
+          { name = "nvim_lsp_signature_help" },                                  -- lsp signature
+          { name = "luasnip" },                                                  -- snippets
+          { name = "buffer" },                                                   -- text within current buffer
+          { name = "path" },                                                     -- file system paths
+          { name = "nerdfont" },                                                 -- nerd font icons
         }),
         -- configure lspkind for vs-code like pictograms in completion menu
         formatting = {
           format = require("lspkind").cmp_format({
             maxwidth = 50,
             ellipsis_char = "...",
+            symbol_map = { Codeium = "", },
           }),
         },
+        experimental = {
+          ghost_text = true,
+        },
+      })
+
+      cmp.setup.filetype("norg", {
+        sources = {
+          { name = "neorg" },
+          { name = "codeium", group_index = 1, priority = 100, },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "nerdfont" },
+        },
+      })
+
+      cmp.setup.filetype("gitcommit", {
+        sources = {
+          { name = "git" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+
+      cmp.setup.filetype("tmux", {
+        sources = {
+          { name = "tmux" },
+          { name = "buffer" },
+          { name = "path" },
+        }
+      })
+
+      cmp.setup.filetype("fish", {
+        sources = {
+          { name = "fish" },
+          { name = "buffer" },
+          { name = "path" },
+        }
+      })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        }
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        }
       })
 
       cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
     end,
   },
   {
-    "windwp/nvim-autopairs",
+    "windwp/nvim-autopairs", -- alternative option = 'cohama/lexima.vim'
     cond = not vim.g.vscode,
     lazy = true,
     opts = {
