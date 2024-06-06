64bac29adf8b36a35d2b5663f01125683b7751a4
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-06-06 15:38:41 -0400

    add arch-chroot calls for each command

--- scripts/MUSE.sh
+++ scripts/MUSE.sh
@@ -42,88 +42,88 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
 # Time
-ln -sf "/usr/share/zoneinfo/$(curl https://ipapi.co/timezone)" /etc/localtime
-hwclock --systohc
-systemctl enable systemd-timesyncd.service
+arch-chroot /mnt ln -sf "/usr/share/zoneinfo/$(curl https://ipapi.co/timezone)" /etc/localtime
+arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt systemctl enable systemd-timesyncd.service
 
 # Locale
-sed -i '/en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen # Uncomment line
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
+arch-chroot /mnt sed -i '/en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen # Uncomment line
+arch-chroot /mnt locale-gen
+arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
+arch-chroot /mnt echo "KEYMAP=us" > /etc/vconsole.conf
 
 # Hostname
-echo "MUSE" > /etc/hostname
+arch-chroot /mnt echo "MUSE" > /etc/hostname
 
 # Initramfs
-mkinitcpio -P
+arch-chroot /mnt mkinitcpio -P
 
 # Root Password
 read -s -p "Root Password: " rootpwd
-echo "root:$rootpwd" | chpasswd
+echo "root:$rootpwd" | arch-chroot /mnt chpasswd
 
 # Add User
 read -s -p "Admin Username: " username
 read -s -p "Admin User Password: " userpwd
-useradd -m -G wheel -s /usr/bin/fish $username
-echo "$username:$userpwd" | chpasswd
-echo "$username ALL=(ALL) ALL" > "/etc/sudoers.d/00_$username"
-chmod 0440 "/etc/sudoers.d/00_$username"
+uarch-chroot /mnt seradd -m -G wheel -s /usr/bin/fish $username
+echo "$username:$userpwd" | arch-chroot /mnt chpasswd
+arch-chroot /mnt echo "$username ALL=(ALL) ALL" > "/etc/sudoers.d/00_$username"
+arch-chroot /mnt chmod 0440 "/etc/sudoers.d/00_$username"
 
 # Boot Loader
-efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode 'initrd=\amd-ucode.img initrd=\initramfs-linux.img root=/dev/nvme0n1p2 rootfstype=ext4 rw quiet splash'
+arch-chroot /mnt efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode 'initrd=\amd-ucode.img initrd=\initramfs-linux.img root=/dev/nvme0n1p2 rootfstype=ext4 rw quiet splash'
 
 # Network Manager
-systemctl enable NetworkManager.service
+arch-chroot /mnt systemctl enable NetworkManager.service
 
 # Full KDE and apps
 # pacman -S --noconfirm plasma-meta kde-applications-meta
 
 # Desktop Environment
-pacman -S --noconfirm plasma-desktop egl-wayland
+arch-chroot /mnt pacman -S --noconfirm plasma-desktop egl-wayland
 # Plasma Apps
-pacman -S --noconfirm kvantum dosfstools ark konsole dolphin partitionmanager kdeconnect
+arch-chroot /mnt pacman -S --noconfirm kvantum dosfstools ark konsole dolphin partitionmanager kdeconnect
 # Additional Packages
-pacman -S --noconfirm openssh nodejs npm xclip unzip zstd
+arch-chroot /mnt pacman -S --noconfirm openssh nodejs npm xclip unzip zstd
 # Terminal tools
-pacman -S --noconfirm eza tmux fzf fd ripgrep zoxide starship ttf-jetbrains-mono-nerd
+arch-chroot /mnt pacman -S --noconfirm eza tmux fzf fd ripgrep zoxide starship ttf-jetbrains-mono-nerd
 # Python tools
-pacman -S --noconfirm python-poetry pyenv
+arch-chroot /mnt pacman -S --noconfirm python-poetry pyenv
 # Graphical apps
-pacman -S --noconfirm wezterm freecad inkscape gimp obsidian
+arch-chroot /mnt pacman -S --noconfirm wezterm freecad inkscape gimp obsidian
 # Candy apps
-pacman -S --noconfirm xcape # kbd fuse2
+arch-chroot /mnt pacman -S --noconfirm xcape # kbd fuse2
 
-systemctl enable sddm.service
+arch-chroot /mnt systemctl enable sddm.service
 
 # Switch users
-su $username
+# su $username
 
 # Yay
-git clone https://aur.archlinux.org/yay.git /tmp/yay && cd /tmp/yay && makepkg -si && yay --version
+arch-chroot -u $username /mnt git clone https://aur.archlinux.org/yay.git /tmp/yay && cd /tmp/yay && makepkg -si && yay --version
 # AUR apps
-yay -S --noconfirm brave-bin neovim-nightly-bin visual-studio-code-bin megasync-bin dolphin-megasync-bin carapace-bin kwin-bismuth
+arch-chroot -u $username /mnt yay -S --noconfirm brave-bin neovim-nightly-bin visual-studio-code-bin megasync-bin dolphin-megasync-bin carapace-bin kwin-bismuth
 # Proton VPN
-yay -S --noconfirm protonvpn network-manager-applet
+arch-chroot -u $username /mnt yay -S --noconfirm protonvpn network-manager-applet
 # AUR extras
-# yay -S teamviewer youtube onedrive-abraunegg logiops displaylink logkeys-git
+# arch-chroot -u $username /mnt yay -S teamviewer youtube onedrive-abraunegg logiops displaylink logkeys-git
 
 # Install Tmux Plugin Manager
-git clone https://github.com/tmux-plugins/tpm ~/.cache/tmux/plugins/tpm
+arch-chroot -u $username /mnt git clone https://github.com/tmux-plugins/tpm ~/.cache/tmux/plugins/tpm
 
 # Python
-pyenv install 3.12
-pyenv global 3.12
+arch-chroot -u $username /mnt pyenv install 3.12
+arch-chroot -u $username /mnt pyenv global 3.12
 
 # SSH
-ssh-keygen
+arch-chroot -u $username /mnt ssh-keygen
 echo "SSH public key:"
-cat ~/.ssh/id_ed25519.pub
+arch-chroot -u $username /mnt cat ~/.ssh/id_ed25519.pub
 read -p "Copy public SSH key and create new key at https://github.com/settings/ssh/new. Press enter when done."
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-ssh -T git@github.com
+arch-chroot -u $username /mnt eval "$(ssh-agent -s)"
+arch-chroot -u $username /mnt ssh-add ~/.ssh/id_ed25519
+arch-chroot -u $username /mnt ssh -T git@github.com
 
 # Dotfiles
-mkdir ~/Code && cd ~/Code && git clone --recurse-submodules git@github.com:kjmcnamara1/dotfiles && cd dotfiles
-./sync.py
+arch-chroot -u $username /mnt mkdir ~/Code && cd ~/Code && git clone --recurse-submodules git@github.com:kjmcnamara1/dotfiles && cd dotfiles
+arch-chroot -u $username /mnt ./sync.py
