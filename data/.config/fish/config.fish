9a3f3285abfd295de66c527568f2b950e3b0f1f4
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-09-10 15:25:08 -0400

    fix yazi wrapper to work with zoxide

--- data/.config/fish/config.fish
+++ data/.config/fish/config.fish
@@ -59,7 +59,7 @@ if status is-interactive # connected to keyboard
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         yazi $argv --cwd-file="$tmp"
         if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            cd -- "$cwd"
+            __zoxide_cd_internal -- "$cwd"
         end
         rm -f -- "$tmp"
     end
