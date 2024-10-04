f447bcad894a9a311b98ab531aa4c15411ee13d5
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-10-04 09:50:52 -0400

    add todo for SyncProfile

--- sync.py
+++ sync.py
@@ -51,6 +51,7 @@ def get_modules(profile_name: str, profiles: dict[str, Any]) -> set[str]:
 @dataclass(frozen=True)
 class SyncModule:
     name: str
+    # TODO: Add SyncProfile via composition
 
     @property
     def path(self) -> Path:
