9cf1e7d682b3a5327bb7d088d6ebce80dee06801
Kevin <kevin.j.mcnamara+tech@proton.me>
2024-12-28 11:47:44 -0500

    add chezmoi plugin

---
+++ nvim-lazyvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,23 @@
+return {
+  'xvzc/chezmoi.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', "ibhagwan/fzf-lua", },
+  keys = {
+    { "<leader>fz", "<cmd>ChezmoiFzf<cr>", desc = "Find Chezmoi File" },
+  },
+  config = function()
+    fzf_chezmoi = function()
+      require('fzf-lua').fzf_exec(require("chezmoi.commands").list(), {
+        actions = {
+          ['default'] = function(selected, opts)
+            require("chezmoi.commands").edit({
+              targets = { "~/" .. selected[1] },
+              args = { "--watch" }
+            })
+          end
+        }
+      })
+    end
+
+    vim.api.nvim_command('command! ChezmoiFzf lua fzf_chezmoi()')
+  end
+}
