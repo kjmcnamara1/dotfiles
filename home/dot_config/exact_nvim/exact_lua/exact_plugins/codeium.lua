4d09d2e5426d2eb3ed62ab0cba0542c03f299a7d
Kevin <kevin.j.mcnamara+tech@proton.me>
2025-01-10 00:13:45 -0500

    add codeium

---
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/codeium.lua
@@ -0,0 +1,39 @@
+return {
+
+  {
+    "Exafunction/codeium.nvim",
+    cmd = "Codeium",
+    event = "InsertEnter",
+    build = ":Codeium Auth",
+    opts = {
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          -- accept = false, -- handled by nvim-cmp / blink.cmp
+          --   next = "<M-]>",
+          --   prev = "<M-[>",
+        },
+      },
+    },
+  },
+
+  -- TODO: don't integrate with blink
+  -- set separate keybind for accept
+  -- change highlight group to distinguish from blink completions
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        compat = { "codeium" },
+        providers = {
+          codeium = {
+            -- kind = "Codeium",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  }
+
+}
