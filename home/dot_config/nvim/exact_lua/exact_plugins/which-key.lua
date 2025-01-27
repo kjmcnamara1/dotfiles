3a4a9b9c15508f70d64fdc56dd0c9dfe8657c691
Kevin <kevin.j.mcnamara+tech@proton.me>
2025-01-27 15:27:02 -0500

    keys

--- home/dot_config/nvim/exact_lua/exact_plugins/which-key.lua
+++ home/dot_config/nvim/exact_lua/exact_plugins/which-key.lua
@@ -9,5 +9,39 @@ return {
     "echasnovski/mini.icons",
   },
   event = "VeryLazy",
-  opts = { preset = "helix" },
+  opts_extend = { "spec" },
+  opts = {
+    preset = "helix",
+    spec = {
+      mode = { "n", "v" },
+      { "<leader><tab>", group = "tabs" },
+      { "<leader>c", group = "code" },
+      { "<leader>f", group = "file/find" },
+      { "<leader>g", group = "git" },
+      { "<leader>gh", group = "hunks" },
+      { "<leader>s", group = "search" },
+      { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+      { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+      { "[", group = "prev" },
+      { "]", group = "next" },
+      { "g", group = "goto" },
+      { "m", group = "surround" },
+      { "z", group = "fold" },
+      {
+        "<leader>b",
+        group = "buffer",
+        expand = function()
+          return require("which-key.extras").expand.buf()
+        end,
+      },
+      {
+        "<leader>w",
+        group = "windows",
+        proxy = "<c-w>",
+        expand = function()
+          return require("which-key.extras").expand.win()
+        end,
+      },
+    },
+  },
 }
