31aa21e68e8b4033d1e80edf6909e85a134604f2
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-02-07 22:38:12 -0500

    make back2dir use separate file instead of Universal fish variable

--- home/dot_config/fish/conf.d/back2dir.fish
+++ home/dot_config/fish/conf.d/back2dir.fish
@@ -1,5 +1,7 @@
-set -q fish_most_recent_dir && [ -d "$fish_most_recent_dir" ] && cd "$fish_most_recent_dir"
+set fish_most_recent_dir_file ~/.config/fish/fish_most_recent_dir
+
+[ -f $fish_most_recent_dir_file ] && [ -d "$(cat $fish_most_recent_dir_file)" ] && cd "$(cat $fish_most_recent_dir_file)"
 
 function __back2dir --on-event fish_exit
-    set -U fish_most_recent_dir $PWD
+    echo $PWD >$fish_most_recent_dir_file
 end
