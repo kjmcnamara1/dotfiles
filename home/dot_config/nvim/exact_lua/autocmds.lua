d7c33682d63b2c2f9a88b1329503f1b622b31d7b
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-02-19 11:24:47 -0500

    add VimResized autocmd

--- home/dot_config/nvim/exact_lua/autocmds.lua
+++ home/dot_config/nvim/exact_lua/autocmds.lua
@@ -19,6 +19,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+vim.api.nvim_create_autocmd("VimResized", {
+  desc = "Resize splits if window got resized",
+  group = CustomGroup,
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Format file on save",
   group = CustomGroup,
