9f8456a9f65b9e194d30fc3a929e6ebb154428d6
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-08-01 14:40:29 -0400

    improve yazi config

--- home/dot_config/yazi/init.lua
+++ home/dot_config/yazi/init.lua
@@ -1,1 +1,9 @@
 require("starship"):setup({ config_file = "~/.config/starship/starship_yazi.toml" })
+
+require("full-border"):setup()
+
+require("git"):setup()
+
+if os.getenv("NVIM") then
+  require("toggle-pane"):entry("min-preview")
+end
