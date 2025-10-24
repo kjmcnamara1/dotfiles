45daab05942c01ce68dc3786c3002bebd0c16555
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-10-23 20:08:18 -0400

    don't notify plugin updates

--- home/dot_config/nvim/exact_lua/exact_config/lazypm.lua
+++ home/dot_config/nvim/exact_lua/exact_config/lazypm.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "onenord" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
   ui = {
     title = "Lazy Plugin Manager",
   },
