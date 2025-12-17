e54bb062f32e15ee5bc41a351f5eb8ccca812215
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-12-17 15:03:12 -0500

    refactor(nixos): use neovim module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -122,10 +122,16 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    # plugins = [ pkgs.vimPlugins.nvim-treesitter ];
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
     curl
