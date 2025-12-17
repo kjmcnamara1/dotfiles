21f8d4fa7b7e65a570b1cde2522945ee4e893b25
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-12-17 14:52:43 -0500

    feat(nixos): enable iwd and bluetooth

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,14 +22,12 @@
   boot.plymouth.enable = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.iwd.enable = true;
+  hardware.bluetooth.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
