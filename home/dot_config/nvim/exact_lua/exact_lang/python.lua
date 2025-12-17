34df1bb010b93e658c53bbdb8b5071ffdb06083b
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2025-12-17 15:16:23 -0500

    feat(nvim): add VenvSelect for xonsh files

--- home/dot_config/nvim/exact_lua/exact_lang/python.lua
+++ home/dot_config/nvim/exact_lua/exact_lang/python.lua
@@ -38,9 +38,8 @@ return {
 
   {
     "linux-cultist/venv-selector.nvim",
-    branch = "regexp", -- Use this branch for the new version
     cmd = "VenvSelect",
-    ft = "python",
+    ft = { "python", "xonsh" },
     keys = {
       { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" }
     },
