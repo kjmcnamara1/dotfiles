c8dabe307433f610edc30ab0e4634fd252966351
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-02 18:11:40 -0500

    WIP: nvim remote

---
+++ home/dot_config/nvim/exact_lua/exact_full/remote.lua
@@ -0,0 +1,22 @@
+return {
+
+  -- {
+  --   "amitds1997/remote-nvim.nvim",
+  --   version = "*",                     -- Pin to GitHub releases
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",         -- For standard functions
+  --     "MunifTanjim/nui.nvim",          -- To build the plugin UI
+  --     "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+  --   },
+  --   config = true,
+  -- },
+
+  -- {
+  --   'chipsenkbeil/distant.nvim',
+  --   branch = 'v0.3',
+  --   config = function()
+  --     require('distant'):setup()
+  --   end
+  -- },
+
+}
