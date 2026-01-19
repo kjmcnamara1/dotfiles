a5cbc250c76e3f78da67c8c9132d4ce36ec3de7f
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-19 14:03:36 -0500

    feat(neovim): add difft.nvim

--- home/dot_config/nvim/exact_lua/exact_full/diffview.lua
+++ home/dot_config/nvim/exact_lua/exact_full/diffview.lua
@@ -17,6 +17,28 @@ return {
         download = true, -- Auto-download pre-built binary
       })
     end,
-  }
+  },
+
+  {
+    "ahkohd/difft.nvim",
+    keys = {
+      {
+        "<leader>d",
+        function()
+          local Difft = require("difft")
+          if Difft.is_visible() then
+            Difft.hide()
+          else
+            Difft.diff()
+          end
+        end,
+        desc = "Toggle Difft",
+      },
+    },
+    opts = {
+      command = "bash -c \"GIT_EXTERNAL_DIFF='difft --color=always' git diff\"", -- or "jj diff --no-pager"
+      layout = "ivy_taller",                                                     -- nil (buffer), "float", or "ivy_taller"
+    },
+  },
 
 }
